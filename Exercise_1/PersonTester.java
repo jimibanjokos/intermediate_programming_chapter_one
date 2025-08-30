5ebdbaa4965f4ded115afdfccf6938bf61ab940f
jimibanjokos <jimibanjokos@gmail.com>
2025-08-30 10:24:38 -0400

    testing the person

---
+++ Exercise_1/PersonTester.java
@@ -0,0 +1,8 @@
+public class PersonTester {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        System.out.println(Person1.toString());
+        person1.setName("Howard Rowark");
+        person1.talk();
+    }
+}
