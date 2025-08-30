a8945001359eeac9433bf02f1d88f552453b0271
jimibanjokos <jimibanjokos@gmail.com>
2025-08-30 10:26:22 -0400

    added comments to PersonTester file

--- Exercise_1/PersonTester.java
+++ Exercise_1/PersonTester.java
@@ -1,3 +1,12 @@
+/**Class: PersonTester
+ * @author: Jimi Banjoko
+ * @version 1.0
+ * Course: ITEC 2150 Fall 2025
+ * Written: August 30, 2025
+ * 
+ * This class - It tests out the person through the variables that were used.
+*/
+
 public class PersonTester {
     public static void main(String[] args) {
         Person person1 = new Person();
