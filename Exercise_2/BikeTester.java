c5d27ead2581f15e30635575bff7b20110cf9aa7
jimibanjokos <jimibanjokos@gmail.com>
2025-08-30 15:27:44 -0400

    created the textBook and TextBookTester file

--- Exercise_2/BikeTester.java
+++ Exercise_2/BikeTester.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 public class BikeTester {
     public static void main(String[] args) {
+        int totalNumberOfWheels;
         Bike bike1 = new Bike();
         System.out.println(bike1.toString());
+        totalNumberOfWheels = bike1.getNumberOfWheels();
         Bike bike2 = new Bike();
         bike2.setNumberOfWheels(4);
+        totalNumberOfWheels += bike2.getNumberOfWheels();
         bike2.setManufacturer("Kevin Mark");
         bike2.setYear(2019);
         System.out.println(bike2.toString());
@@ -12,6 +15,8 @@ public class BikeTester {
         bikeList.add(bike1);
         bikeList.add(bike2);
         System.out.println(bikeList);
+        System.out.println("The total number of wheels is " + totalNumberOfWheels);
+
 
     }
 }
