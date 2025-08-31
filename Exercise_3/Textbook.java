55cf58315235a0d73b7b68eea89ca29117f900a9
jimibanjokos <jimibanjokos@gmail.com>
2025-08-30 23:53:36 -0400

    added variables, constructors, getter methods, setter methods, and toString method

---
+++ Exercise_3/Textbook.java
@@ -0,0 +1,46 @@
+class TextBook {
+    private String title;
+    private String author;
+    private int numberOfPages;
+
+    public TextBook() {
+        this.title = "Wolves in the Forest";
+        this.author = "Ben Frank";
+        this.numberOfPages = 32;
+    }
+
+    public TextBook(String title, String author, int numberOfPages) {
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", Number of Pages: " + numberOfPages;
+    }
+    
+}
