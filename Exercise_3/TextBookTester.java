0d889f7ec26dda814f672d4c08178543841d8668
jimibanjokos <jimibanjokos@gmail.com>
2025-08-31 18:18:50 -0400

    added comments for TextBookTester

--- Exercise_3/TextBookTester.java
+++ Exercise_3/TextBookTester.java
@@ -1,3 +1,12 @@
+/**Class: TextBookTester
+ * @author: Jimi Banjoko
+ * @version 1.0
+ * Course: ITEC 2150 Fall 2025
+ * Written: August 30, 2025
+ * 
+ * This class - It tests out the textbook through the variables that were used.
+*/
+
 public class TextBookTester {
     public static void main(String[] args) {
        TextBook textBookOne = new TextBook();
